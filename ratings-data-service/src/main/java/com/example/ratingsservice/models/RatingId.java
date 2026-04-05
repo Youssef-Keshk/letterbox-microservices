f5586e3b23ed1b4b72742208c2fd16e9e0791f23
Youssef-Keshk <youssefkeshk001@gmail.com>
2026-04-05 15:30:00 +0200

    add MovieAggregate and RatingId models with JPA repositories for MySQL persistence

---
+++ ratings-data-service/src/main/java/com/example/ratingsservice/models/RatingId.java
@@ -0,0 +1,36 @@
+package com.example.ratingsservice.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class RatingId implements Serializable {
+    private String userId;
+    private String movieId;
+
+    public RatingId() {}
+
+    public RatingId(String userId, String movieId) {
+        this.userId = userId;
+        this.movieId = movieId;
+    }
+
+    // Getters and Setters
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+    public String getMovieId() { return movieId; }
+    public void setMovieId(String movieId) { this.movieId = movieId; }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RatingId ratingId = (RatingId) o;
+        return Objects.equals(userId, ratingId.userId) && Objects.equals(movieId, ratingId.movieId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, movieId);
+    }
+}
